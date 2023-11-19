38a472f2a45e0576a2200828f437cd3e2f842986
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-11-20 00:51:24 +0300

    island model created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,17 @@ import Loader from '../components/Loader'
       </div> */}
 function Home() {
   return (
-    <section className='w-full h-screen relative'>
+    // w-full h-screen relative
+    <section className=''>
       <Canvas
         className='w-full h-screen bg-transparent'
         camera={{near: 0.1, far: 1000}}>
           <Suspense fallback={<Loader />}>
-
+            <directionalLight/>
+            <ambientLight/>
+            <pointLight/>
+            <spotLight/>
+            <hemisphereLight/>
           </Suspense>
       </Canvas>
     </section>
