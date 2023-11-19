d0ac4f0d3f7b1d0fc5238a8eb35ce8bba48cacf9
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-11-19 16:41:06 +0300

    home page changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,13 +10,12 @@ function Home() {
       <Canvas
         className='w-full h-screen bg-transparent'
         camera={{near: 0.1, far: 1000}}>
-          <Suspense fallback={<Loader/>}>
+          <Suspense fallback={<Loader />}>
 
           </Suspense>
       </Canvas>
     </section>
   )
 }
-import Loader from '../components/Loader';
 
 export default Home
