593963ee16b3c0d91148c4bd24ce59c8c705dbe6
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-11-20 01:25:36 +0300

    island layout implemented

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -1,42 +1,45 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useGLTF } from '@react-three/drei';
+import { useFrame, useThree } from '@react-three/fiber';
+import { a } from '@react-spring/three';
 
-// import islandScene from '../assets/3d/island.glb';
+import islandScene from '../assets/3d/island.glb';
 
 const Island = (props) => {
-    const { nodes, materials } = useGLTF();('/island.glb');
+    const islandRef = useRef();
+    const { nodes, materials } = useGLTF(islandScene);
 
     return (
         // {Island 3D model from: https://sketchfab.com/3d-models/foxs-islands-163b68e09fcc47618450150be7785907}
-        <a.group ref={islandRef} {...props}>
-        <mesh
-        geometry={nodes.polySurface944_tree_body_0.geometry}
-        material={materials.PaletteMaterial001}
-        />
-        <mesh
-        geometry={nodes.polySurface945_tree1_0.geometry}
-        material={materials.PaletteMaterial001}
-        />
-        <mesh
-        geometry={nodes.polySurface946_tree2_0.geometry}
-        material={materials.PaletteMaterial001}
-        />
-        <mesh
-        geometry={nodes.polySurface947_tree1_0.geometry}
-        material={materials.PaletteMaterial001}
-        />
-        <mesh
-        geometry={nodes.polySurface948_tree_body_0.geometry}
-        material={materials.PaletteMaterial001}
-        />
-        <mesh
-        geometry={nodes.polySurface949_tree_body_0.geometry}
-        material={materials.PaletteMaterial001}
-        />
-        <mesh
-        geometry={nodes.pCube11_rocks1_0.geometry}
-        material={materials.PaletteMaterial001}
-        />
+        <a.group ref={islandRef} {...props} dispose={null}>
+            <mesh
+            geometry={nodes.polySurface944_tree_body_0.geometry}
+            material={materials.PaletteMaterial001}
+            />
+            <mesh
+            geometry={nodes.polySurface945_tree1_0.geometry}
+            material={materials.PaletteMaterial001}
+            />
+            <mesh
+            geometry={nodes.polySurface946_tree2_0.geometry}
+            material={materials.PaletteMaterial001}
+            />
+            <mesh
+            geometry={nodes.polySurface947_tree1_0.geometry}
+            material={materials.PaletteMaterial001}
+            />
+            <mesh
+            geometry={nodes.polySurface948_tree_body_0.geometry}
+            material={materials.PaletteMaterial001}
+            />
+            <mesh
+            geometry={nodes.polySurface949_tree_body_0.geometry}
+            material={materials.PaletteMaterial001}
+            />
+            <mesh
+            geometry={nodes.pCube11_rocks1_0.geometry}
+            material={materials.PaletteMaterial001}
+            />
         </a.group>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,26 @@
 import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber';
 import Loader from '../components/Loader'
+import Island from '../models/Island';
 {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
         POPUP
       </div> */}
 function Home() {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null
+    let screenPosition = [0, -6.5, -43]
+    let rotation = [0.1, 4.7, 0]
+
+    if(window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+    return [screenScale, screenPosition, rotation];
+  }
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
   return (
     // w-full h-screen relative
     <section className=''>
@@ -17,6 +33,12 @@ function Home() {
             <pointLight/>
             <spotLight/>
             <hemisphereLight/>
+
+            <Island 
+              position={islandPosition}
+              scale={islandScale}
+              rotation={islandRotation}
+            />
           </Suspense>
       </Canvas>
     </section>
