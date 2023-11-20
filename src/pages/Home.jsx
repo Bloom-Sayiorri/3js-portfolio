7f5d60301a75f223440a7f0fa698586b70604b59
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-11-20 23:28:31 +0300

    eslintrc chsnges

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ function Home() {
   }
 
   const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
-
+  
   return (
     <section className='w-full h-screen relative'>
       <Navbar />
@@ -29,7 +29,7 @@ function Home() {
         className='w-full h-screen bg-transparent'
         camera={{near: 0.1, far: 1000}}>
           <Suspense fallback={<Loader />}>
-            <directionalLight/>
+            <directionalLight position={[1, 1, 1]} intensity={[2]}/> 
             <ambientLight/>
             <pointLight/>
             <spotLight/>
