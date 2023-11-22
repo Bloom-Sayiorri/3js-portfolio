5df36577d12a7b4b6c27eef926aca07d5f30a90e
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2023-11-22 16:36:33 +0300

    changes

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import birdScene from '../assets/3d/bird.glb';
+import { useGLTF } from '@react-three/drei';
+
 const Bird = () => {
+    const { scene, animations } = useGLTF(birdScene);
   return (
-    <div>Bird</div>
+    <mesh>
+
+    </mesh>
   )
 }
 
--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import planeScene from '../assets/3d/plane.glb';
+import { useGLTF } from '@react-three/drei';
+
+
 const Plane = () => {
+    const { scene, animations } = useGLTF(birdscene);
   return (
-    <div>Plane</div>
+    <mesh>
+
+    </mesh>
   )
 }
 
